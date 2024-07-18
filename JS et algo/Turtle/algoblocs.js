620f930424faf8fcdaed8c59be572d824c9d76c2
jiji <168441188+JijiSom@users.noreply.github.com>
2024-07-18 13:39:09 +0200

    Add files via upload

---
+++ JS et algo/Turtle/algoblocs.js
@@ -0,0 +1,166 @@
+/*
+//A1
+//INITIALISATION
+setPos(300,300);
+changeColor(color.red);
+setLineWidth(10);
+faceDown();
+
+//Réalisation 
+forward(200);
+faceRight();
+forward(100); 
+
+*/
+/*
+//a2 
+// initialisation  
+
+setPos(300,200);
+changeColor(color.yellow);
+setLineWidth(5); 
+
+//réalisation 
+
+faceLeft();
+forward(50);
+faceDown();
+forward(120);
+faceRight();
+forward(200);
+faceUp();
+forward(200); 
+*/
+
+/*
+//a3 
+//initialisation 
+setPos(200,400);
+changeColor(color.green);
+setLineWidth(10);
+faceRight();
+//Réalisation 
+forward(100);
+arcLeft(50,180);
+forward(50);
+arcRight(50,180);
+forward(100);
+*/
+
+/*
+//a4
+//initialisation 
+setPos(200,180)
+changeColor(color.green);
+setLineWidth(10);
+
+//Réalisation
+forward(100);
+arcLeft(50,180);
+forward(50);
+up();
+forward(50);
+down()
+forward(100);
+arcLeft(50,180);
+forward(50);
+*/
+/*
+
+//B1
+//initialisation
+setPos(150,250);
+changeColor(color.green);
+setLineWidth(15);
+faceRight();
+
+
+// Réalisation
+for(let i = 0 ; i < 4 ; i++) {
+    forward(100);
+    faceDown();
+    forward(100);
+    faceRight();
+}
+*/
+/*
+//b10
+//initialisation 
+
+setPos(150,250);
+changeColor(color.blue);
+setLineWidth(15);
+
+//Réalisation
+
+for(let i = 0 ; i < 8 ; i++) {
+    
+    right(180-45);
+    forward(100);
+}
+up();
+forward(200);
+changeColor(color.yellow);
+down();
+
+for(let i = 0 ; i < 8 ; i++) {
+    forward(100);
+    right(180-45);
+
+}
+right(90);
+up()
+forward(50);
+forward(50);
+changeColor(color.fuchsia)
+down();
+
+for(let i = 0 ; i < 8 ; i++) {
+    forward(100);
+    right(180-45);
+}
+*/
+/*
+// b17
+//Initialisation 
+setPos(200,180)
+changeColor(color.blue);
+setLineWidth(10);
+
+//Réalisation
+for(let i = 0 ; i < 8 ; i++) {
+    arcLeft(70,180); 
+    left(120,60);
+}
+    */
+
+/*
+//b12 
+//initialisation
+setPos(200,400)
+setLineWidth(10);
+faceUp();
+
+//Réalisation
+forward(200);
+changeColor(color.red);
+for (let i = 0 ; i < 3 ; i++){
+    forward(100);
+    right(120);
+}
+    */
+
+//b18
+//initialisation 
+setPos(300,150);
+setLineWidth(10);
+changeColor(color.yellow);
+
+//Réalisation
+
+for (let i = 0 ; i < 8 ; i++){
+  forward(400);
+   left(90);
+   forward(100);
+   left(120); 
+}
---
+++ JS et algo/fonction/fonctions.js
@@ -0,0 +1,61 @@
+// Écrire une fonction f1 qui ne prend rien en entrée, et qui affiche 17 dans la console.
+function f1() {
+    console.log(17);
+}
+
+// Utiliser la fonction f1 pour afficher 17 dans la console.
+f1();
+// Écrire une fonction f2 qui ne prend rien en entrée, et qui retourne 18.
+function f2() {
+  return 18;
+} 
+// Utiliser la fonction f2 pour afficher 18 dans la console.
+f2();
+// La fonction f2 a l'air moins pratique à utiliser. Quel intérêt pourrait-elle avoir par rapport à f1 ?
+
+// Écrire une fonction f3 qui prend un nombre en entrée et qui affiche dans la console le double de ce nombre.
+function f3(number) {
+let v =  double(10); 
+return v; 
+}
+// Utiliser la fonction f3 pour écrire dans la console le double de 97.
+function f3 () {
+    console.log(97*2);
+}
+// Écrire une fonction f4 qui prend un nombre en entrée et qui retourne le double de ce nombre.
+function f4( number) {
+    let v12 = 10;
+    return v12; 
+
+}
+// Utiliser la fonction f4 pour écrire dans la console le double de 98.
+
+// Utiliser la fonction f4 pour stocker dans une variable appelée a le double de 99.
+
+// Écrire une fonction f5 qui prend en entrée deux nombres et qui affiche dans la console la somme de ces deux nombres.
+function f3(){
+    let v13 = 14;
+    let v14 = 15;
+
+console.log (14+15);
+
+}
+
+
+
+// Utiliser la fonction f5 pour écrire dans la console la somme de 43 et 76.
+function bella () {
+
+}
+// Écrire une fonction f6 qui prend en entrée deux nombres et qui retourne la somme de ces deux nombres.
+
+// Utiliser la fonction f6 pour écrire dans la console la somme de 41 et 78.
+
+// Utiliser la fonction f6 pour incrémenter la variable a de la somme de 42 et 77.
+
+// Écrire une fonction f7 qui prend deux nombres en entrée et qui retourne le plus grand des deux.
+
+// Écrire une fonction f8 qui prend trois nombres en entrée et qui retourne le plus grand des trois.
+// (Au moins deux versions possibles : sans utiliser f7, et en utilisant f7).
+
+// Écrire une fonction f9 qui prend un nombre et un mot en entrée, et qui affiche dans la console ce mot ce nombre de fois.
